d3247d6fc5264bd97399d40832fcec87d7bdd73f
Vipul <vipulg14@gmail.com>
2023-01-08 01:39:29 +0530

    create video added

--- app/Jobs/Content/ConvertSlideToVideo.php
+++ app/Jobs/Content/ConvertSlideToVideo.php
@@ -2,12 +2,15 @@
 
 namespace App\Jobs\Content;
 
+use App\Models\Contents;
+use App\Models\ContentSlides;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 
 class ConvertSlideToVideo implements ShouldQueue
 {
@@ -34,6 +37,37 @@ class ConvertSlideToVideo implements ShouldQueue
      */
     public function handle()
     {
-        //Slides processing here
+        $slides = ContentSlides::where([
+            'id' => $this->slide_id
+        ])->first();
+        
+        $folder = "slide_".$this->slide_id."_".uniqid();
+        $directory = "/$folder/";
+        $storage_path = storage_path();
+        $input = "action=merge&resource_id=$folder&storage_path=$storage_path";
+
+        $allSlides = json_decode($slides->slides, true);
+        foreach ($allSlides as $key => $value) {
+            Storage::disk('local')->put($directory.$value["sequence"].".mp3", file_get_contents($value["audio_file"]));
+            Storage::disk('local')->put($directory.$value["sequence"].".jpg", file_get_contents($value["media_file"]));
+        }
+        $app_path = app_path();
+        // var_dump(storage_path());
+        $command = "/usr/bin/php $app_path/Http/Controllers/v1/Ffmpeg.php \"$input\"";
+        // echo $command;exit;
+        exec ($command);
+        // sleep(5);
+        $path = $directory."final.mp4";
+        $fileExists = Storage::disk('local')->exists($path);
+        $local_path = Storage::url('app'.$path);
+        if($fileExists){
+            $content = Contents::where([
+                'id' => $slides->content_id,
+            ])->first();
+            if($content && empty($content->media_url)){
+                $content->media_url = $local_path;
+                $content->save();
+            }
+        }
     }
 }
